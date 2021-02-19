166a53938ad8c6e8a570d0d7e745ba055d02d048
echoaiyaya <echoaiyaya@gmail.com>
2021-02-19 07:46:51 -0500

    user pages

---
+++ views/js/index.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+	$('.carousel').carousel();
+	let navBtn = document.getElementById('navBtn');
+	let navList = document.getElementById('navList');
+	navBtn.onclick = function() {
+		let key = navList.style.display;
+		if (key == '' || key == 'none') {
+			navList.style.display = 'block';
+		} else {
+			navList.style.display = 'none';
+		}
+	}
+}
